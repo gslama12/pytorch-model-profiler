e8c169d5a9612927eb7c0a5f5443ea0a5ea0793f
Georg Slamanig <georgslamanig@gmail.com>
2024-10-22 10:38:41 +0200

    tried to fix module not found error

--- model_profiler/profilers/__init__.py
+++ model_profiler/profilers/__init__.py
@@ -1,3 +1,4 @@
 from model_profiler.profilers.memory_profiler import profile_memory_cost
+from model_profiler.profilers.flops_profiler import FlopCounterMode
 
 __all__ = ["FlopCounterMode", "profile_memory_cost"]
